6700ef20ec56ddcc02ef3731cec2e0a35d710f08
Ondrej Zara <ondrej.zara@gmail.com>
2014-12-26 23:04:21 +0100

    ui

--- js/jukebox.js
+++ js/jukebox.js
@@ -8,29 +8,47 @@ var Jukebox = {
 	_data: null,
 	_analyser: null,
 	_lastValue: 0,
-	_node: document.createElement("div"),
-
+	_playlistIndex: -1,
+	_playlist: [
+		{name:"ZZ Top &ndash; Gimme All Your Lovin'", file:"zz.ogg", threshold:1.5},
+		{name:"Peter Gabriel &ndash; Sledge Hammer", file:"hammer.ogg", threshold:2},
+		{name:"Lenny Kravitz &ndash; American Woman", file:"woman.ogg", threshold:2},
+		{name:"Phatt Bastard &amp; The Drummachine", file:"phatt.ogg", threshold:1},
+		{name:"Primal Scream &ndash; Rocks", file:"rocks.ogg", threshold:2},
+		{name:"Propellerheads &ndash; History Repeating", file:"history.ogg", threshold:2},
+		{name:"Mousse T. &ndash; Horny", file:"horny.ogg", threshold:2.5},
+		{name:"Tom Jones &ndash; Sexbomb", file:"sexbomb.ogg", threshold:1.5}
+	],
+	
 	handleEvent: function(e) {
 		switch (e.type) {
 			case "load":
 				this._init();
 			break;
 			
-			case "change":
-				this._audio.src = "ogg/" + e.target.value;
-				this._audio.play();
-			break;
-			
-			case "playing":
-				/* fixme mode resolution */
-				var s = this._node.querySelector("select");
-				document.querySelector("#banner").innerHTML = s.options[s.selectedIndex].innerHTML;
-			break;
-			
 			case "ended":
 				document.querySelector("#banner").innerHTML = "";
-				/* fixme only in playlist */
-				this._next();
+				if (this._playlistIndex > -1) { this._next(); }
+ 			break;
+			
+			case "submit":
+				e.preventDefault();
+				if (e.target.id != "url") { return; }
+				var url = document.querySelector("#url input");
+				if (!url.value) { return; }
+				this._playUrl(url.value);
+			break;
+			
+			case "change":
+				if (!e.target.files.length) { return; }
+				var file = e.target.files[0];
+				if (!this._testType(file.type)) { return; }
+				var url = URL.createObjectURL(file);
+				this._play(url, file.name);
+			break;
+			
+			case "click":
+				this["_" + e.target.id]();
 			break;
 		}
 	},
@@ -43,13 +61,10 @@ var Jukebox = {
 			return;
 		}
 
-		document.body.appendChild(this._node);
-		this._node.id = "jukebox";
 		this._audio.addEventListener("ended", this);
-		this._audio.addEventListener("playing", this);
 		
 		this._analyser = this._ctx.createAnalyser();
-		this._analyser.fftSize = 32;
+		this._analyser.fftSize = 2048;
 		this._analyser.smoothingTimeConstant = 0.5
 		this._analyser.connect(this._ctx.destination);
 
@@ -57,11 +72,11 @@ var Jukebox = {
 		source.connect(this._analyser);
 		this._data = new Uint8Array(this._analyser.frequencyBinCount);
 
-		this._buildSelect();
+		this._build();
 		this._next();
 
 		this._tick = this._tick.bind(this);
-		setInterval(this._tick, 17);
+		setInterval(this._tick, 50);
 		
 		for (var i=0;i<C.length;i++) {
 			var canvas = document.createElement("canvas");
@@ -73,37 +88,73 @@ var Jukebox = {
 		}
 	},
 	
-	_buildSelect: function() {
-		var songs = [
-			{name:"ZZ Top &ndash; Gimme All Your Lovin'", file:"zz.ogg", threshold:1.5},
-			{name:"Peter Gabriel &ndash; Sledge Hammer", file:"hammer.ogg", threshold:2},
-			{name:"Lenny Kravitz &ndash; American Woman", file:"woman.ogg", threshold:2},
-			{name:"Phatt Bastard &amp; The Drummachine", file:"phatt.ogg", threshold:1},
-			{name:"Primal Scream &ndash; Rocks", file:"rocks.ogg", threshold:2},
-			{name:"Propellerheads &ndash; History Repeating", file:"history.ogg", threshold:2},
-			{name:"Mousse T. &ndash; Horny", file:"horny.ogg", threshold:2.5},
-			{name:"Tom Jones &ndash; Sexbomb", file:"sexbomb.ogg", threshold:1.5}
-		];
+	_build: function() {
+		var forms = [].slice.call(document.querySelectorAll("#jukebox form"));
+		forms.forEach(function(form) {
+			form.addEventListener("submit", this);
+		}, this);
 		
-		var s = document.createElement("select");
-		s.addEventListener("change", this);
-		this._node.appendChild(s);
+		document.querySelector("#jukebox #prev").addEventListener("click", this);
+		document.querySelector("#jukebox #next").addEventListener("click", this);
+		document.querySelector("#jukebox #file input").addEventListener("change", this);
+		document.querySelector("#jukebox #total").innerHTML = this._playlist.length;
+	},
+	
+	_playUrl: function(url) {
+		var xhr = new XMLHttpRequest();
+		xhr.open("head", url, true);
+		
+		xhr.addEventListener("load", function() {
+			var ct = xhr.getResponseHeader("Content-Type");
+			if (!this._testType(ct)) { return; }
+			this._play(url);
+		}.bind(this));
 
-		songs.forEach(function(song) {
-			var o = document.createElement("option");
-			o.value = song.file;
-			o.innerHTML = song.name;
-			s.appendChild(o);
+		xhr.addEventListener("error", function(e) {
+			var str = "Sorry, this URL cannot be opened.\n\n";
+			if (xhr.status) {
+				str += "HTTP/" + xhr.status;
+			} else {
+				str += "Please make sure that the resource is served with proper CORS headers.";
+			}
+			alert(str);
 		});
-		s.selectedIndex = -1;
+
+		try {
+			xhr.send();
+		} catch (e) {
+			alert("Sorry, this URL cannot be opened (" + e.message + ")");
+		}
+	},
+	
+	_testType: function(type) {
+		if (!this._audio.canPlayType(type)) {
+			alert("Sorry, this file type (" + type + ") is not supported by your browser.");
+			return false;
+		} else {
+			return true;
+		}
+	},
+	
+	_play: function(url, name) {
+		this._audio.src = url;
+		this._audio.play();
+		document.querySelector("#banner").innerHTML = name || url;
 	},
 	
 	_next: function() {
-		var s = this._node.querySelector("select");
-		var i = (s.selectedIndex+1) % s.options.length;
-		s.selectedIndex = i;
-		this._audio.src = "ogg/" + s.value;
-		this._audio.play();
+		this._playList(this._playlistIndex+1);
+	},
+	
+	_prev: function() {
+		this._playList(this._playlistIndex-1);
+	},
+	
+	_playList: function(index) {
+		this._playlistIndex = (index + this._playlist.length) % this._playlist.length;
+		document.querySelector("#jukebox #current").innerHTML = this._playlistIndex+1;
+		var item = this._playlist[this._playlistIndex];
+		this._play("ogg/" + item.file, item.name);
 	},
 	
 	_tick: function() {
@@ -162,6 +213,8 @@ var Jukebox = {
 		deltas.push([
 			this._data[0]/tmp[0] || 0,
 			this._data[1]/tmp[1] || 0,
+			this._data[2]/tmp[2] || 0,
+			this._data[3]/tmp[3] || 0,
 			this._data[11]/tmp[11] || 0,
 			this._data[12]/tmp[12] || 0,
 			this._data[13]/tmp[13] || 0,
